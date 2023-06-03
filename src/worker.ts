f99e22e94f8689ff92d3dd0befbe3de94b379ea2
Adishwar Rishi <adiswa123@gmail.com>
2023-06-04 03:16:10 +1000

    Add OTEL

--- src/worker.ts
+++ src/worker.ts
@@ -1,32 +1,21 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { PartialTraceConfig, instrument } from '@microlabs/otel-cf-workers';
 
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
-}
+export interface Env {}
 
-export default {
+const handler = {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response('Hello World!');
+    return new Response(JSON.stringify({ hello: 'world' }), {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
   },
 };
+
+const config: PartialTraceConfig = {
+  exporter: { url: 'https://api.honeycomb.io' },
+  service: { name: 'worker-greetings' },
+};
+
+export default instrument(handler, config);
