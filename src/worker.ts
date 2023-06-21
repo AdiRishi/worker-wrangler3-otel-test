83a844bb99c5a5b92de8d61315f7cd3e5c297ed5
Adishwar Rishi <adiswa123@gmail.com>
2023-06-21 11:51:00 +1000

    Fix trace uri

--- src/worker.ts
+++ src/worker.ts
@@ -4,6 +4,7 @@ export interface Env {}
 
 const handler = {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const req = await fetch('https://www.google.com');
     return new Response(JSON.stringify({ hello: 'world' }), {
       status: 200,
       headers: {
@@ -15,12 +16,12 @@ const handler = {
 
 const config: TraceConfig= {
   exporter: {
-    url: 'https://api.honeycomb.io',
+    url: 'https://api.honeycomb.io/v1/traces',
     headers: { // use the simple way for testing
       'x-honeycomb-team': 'SECRET',
     },
   },
-  service: { name: 'worker-greetings' },
+  service: { name: 'worker-mf3-greetings' },
 };
 
 export default instrument(handler, config);
